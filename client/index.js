be99f8e029d6269ef51f3edc5323588ebdba65d9
jdidi94 <jdididaoud404@gmail.com>
2021-03-03 12:49:46 +0100

    last-attempt

--- client/index.js
+++ client/index.js
@@ -2,4 +2,4 @@ import Example from './components/Review-list.jsx';
 import reactDOM from 'react-dom'
 // window.NameOfTheService = Example;
 
-reactDOM.render(<Example/>, document.getElementById('name-of-the-service'))
+reactDOM.render(<Example/>, document.getElementById('reviews'))
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3004;
 const env = require("dotenv").config();
 const axios = require("axios");
 
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/api/reviews", async (req, res) => {
   try {
-    const Authorization = process.env.token_key;
+    const Authorization = "a20487f403954c245e367c9dd6baa3c31a02d7aa";
     const data = await axios.get(
       "https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews?product_id=11135",
       {
